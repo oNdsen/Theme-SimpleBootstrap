dfc9600a2fa74d5650a9fb469476ec7f892bcedb
Andreas Reinhard <a.reinhard@me.com>
2017-04-25 14:50:27 +0200

    Restyled Extras Page

---
+++ SBS/modules/extras/extras.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+        $('.dragbox-content').html(function(index, text) {
+                return text.replace(/\ - /g, '');
+        });
+
+        $('[href^="#uninstall_"]').addClass('btn').addClass('btn-warning').addClass('btn-sm').removeAttr('style');
+        $('[href^="#remove_"]').addClass('btn').addClass('btn-danger').addClass('btn-sm').removeAttr('style');
+	$('[href^="#install_"]').addClass('btn').addClass('btn-success').addClass('btn-sm').removeAttr('style');
+        $('[href^="?m=extras&searchForUpdates="]').addClass('btn').addClass('btn-primary').addClass('btn-sm').removeAttr('style');
+
+	$('.dragbox-content').prepend('<br>');
+	$('.dragbox-content br').each(function(){
+		var $set = $();
+		var nxt = this.nextSibling;
+		while(nxt) {
+			if(!$(nxt).is('br')) {
+				$set.push(nxt);
+				nxt = nxt.nextSibling;
+			} else break;
+		}
+		$set.wrapAll('<div class="content" />');
+	});
+	$('.dragbox-content br').remove();
+
+	$('b[style^="color:"]').addClass('badge').addClass('badge-default').removeAttr('style');
+
+
+});
+
