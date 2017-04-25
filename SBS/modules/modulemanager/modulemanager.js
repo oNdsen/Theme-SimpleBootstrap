5e28ced486f3d1e3855d147bac25e2efe75e7e45
Andreas Reinhard <a.reinhard@me.com>
2017-04-25 14:51:04 +0200

    Added some Button Styles on Modules Page

---
+++ SBS/modules/modulemanager/modulemanager.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$('[href^="?m=modulemanager&p=del&id="]').addClass('btn').addClass('btn-danger').addClass('btn-sm');
+	$('[href^="?m=modulemanager&p=add&module="]').addClass('btn').addClass('btn-success').addClass('btn-sm');
+});
