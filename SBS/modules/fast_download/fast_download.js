184b99b0b9955f103d72edebcb08a994f336b648
Andreas Reinhard <a.reinhard@me.com>
2017-05-15 15:09:37 +0200

    Added FastDL Styles and putting the Content into a Table

---
+++ SBS/modules/fast_download/fast_download.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$('.success').addClass('alert').addClass('alert-success');
+	$('.failure').addClass('alert').addClass('alert-danger');
+
+	$('.main [href^="?m=fast_download"]').addClass('btn').addClass('btn-sm').addClass('btn-primary');
+	$('input[name="delete"], input[name="stop_fastdl"]').removeClass('btn-primary').addClass('btn-danger');
+
+
+	var form_selector = $('.main > form:nth-child(5)');
+	$(form_selector).prepend('<br>');
+        $(form_selector.find('br')).each(function(){
+                var $set = $();
+                var nxt = this.nextSibling;
+                while(nxt) {
+                        if(!$(nxt).is('br')) {
+                                $set.push(nxt);
+                                nxt = nxt.nextSibling;
+                        } else break;
+                }
+                $set.wrapAll('<tr><td></td></tr>');
+        });
+        $(form_selector.find('br')).remove();
+	$(form_selector.find('tr')).wrapAll('<table class="table table-sm table-striped" />');
+
+
+});
