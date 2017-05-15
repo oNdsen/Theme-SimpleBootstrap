40f6e6365f0ab3f90cbc4130bdff7608d25ff733
Andreas Reinhard <a.reinhard@me.com>
2017-05-15 15:44:47 +0200

    Replaced Images with Icons

---
+++ SBS/modules/TS3Admin/TS3Admin.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$('img[src$="edit.png"]').replaceWith('<i class="fa fa-pencil-square-o" aria-hidden="true"></i>');
+	$('img[src$="refresh.png"]').replaceWith('<i class="fa fa-refresh" aria-hidden="true"></i>');
+});
