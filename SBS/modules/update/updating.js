a2cd3a3edd4bd797ded6c29c0a91ac458cded603
Andreas Reinhard <a.reinhard@me.com>
2017-05-15 15:09:05 +0200

    Added Update Styles

--- SBS/modules/update/updating.js
+++ SBS/modules/update/updating.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
         $('.success').addClass('alert').addClass('alert-success');
         $('.failure').addClass('alert').addClass('alert-danger');
 
-	$('.main [href="?m=update&p=blacklist"]').addClass('btn').addClass('btn-sm').addClass('btn-primary');
+
 });
