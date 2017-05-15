a331226e418acf225674b81c840d36523711a5c3
Andreas Reinhard <a.reinhard@me.com>
2017-05-15 15:08:32 +0200

    Putting the Extras Content into a Table instead of the Div's on each Row

--- SBS/modules/extras/extras.js
+++ SBS/modules/extras/extras.js
@@ -8,19 +8,23 @@ $(document).ready(function() {
 	$('[href^="#install_"]').addClass('btn').addClass('btn-success').addClass('btn-xs').removeAttr('style');
         $('[href^="?m=extras&searchForUpdates="]').addClass('btn').addClass('btn-primary').addClass('btn-xs').removeAttr('style');
 
-	$('.dragbox-content').prepend('<br>');
-	$('.dragbox-content br').each(function(){
-		var $set = $();
-		var nxt = this.nextSibling;
-		while(nxt) {
-			if(!$(nxt).is('br')) {
-				$set.push(nxt);
-				nxt = nxt.nextSibling;
-			} else break;
-		}
-		$set.wrapAll('<div class="content" />');
+	$('.dragbox-content').each(function(){
+        	var form_selector = $(this);
+        	$(form_selector).prepend('<br>');
+        	$(form_selector.find('br')).each(function(){
+        	        var $set = $();
+        	        var nxt = this.nextSibling;
+        	        while(nxt) {
+        	                if(!$(nxt).is('br')) {
+        	                        $set.push(nxt);
+        	                        nxt = nxt.nextSibling;
+        	                } else break;
+        	        }
+        	        $set.wrapAll('<tr><td></td></tr>');
+        	});
+        	$(form_selector.find('br')).remove();
+        	$(form_selector.find('tr')).wrapAll('<table class="table table-sm table-striped" />');
 	});
-	$('.dragbox-content br').remove();
 
         $('b[style="color:green;"').addClass('label').addClass('label-success').css("color", "");
         $('b[style="color:red;"').addClass('label').addClass('label-danger').css("color", "");
