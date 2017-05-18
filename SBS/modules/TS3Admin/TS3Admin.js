d5083c52f34c829f2cca213f2beeb64c96514f76
Andreas Reinhard <a.reinhard@me.com>
2017-05-18 16:37:15 +0200

    Added red Button for Deletion

--- SBS/modules/TS3Admin/TS3Admin.js
+++ SBS/modules/TS3Admin/TS3Admin.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
 	$('img[src$="edit.png"]').replaceWith('<i class="fa fa-pencil-square-o" aria-hidden="true"></i>');
 	$('img[src$="refresh.png"]').replaceWith('<i class="fa fa-refresh" aria-hidden="true"></i>');
+
+        $('[name="stopvServer"]').removeClass('btn-primary').addClass('btn-danger');
+
 });
+
