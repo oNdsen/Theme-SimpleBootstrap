2a554f3c5de8d014f18aae403c0ad1cafc25ed16
oNdsen <a.reinhard@me.com>
2017-05-19 22:18:11 +0200

    News Module Cleanup

--- SBS/modules/news/admin_news.js
+++ SBS/modules/news/admin_news.js
@@ -2,4 +2,36 @@ $(document).ready(function() {
 	$('#description').addClass('form-control');
 	$('.underline-link, .news-btn').addClass('btn').addClass('btn-primary').addClass('btn-sm');
 	$('.ajax-upload-dragdrop input').removeAttr('style');
+
+	create_button('[href$="&page=permissions"]', 'btn-danger');
+	create_button('[href$="&page=settings"]', 'btn-warning');
+	create_button('[href$="&page=add"]', 'btn-success');
+
+        /* *** Replace UL / LI with Table *** */
+        var ul = $(".main ol");
+        $(ul).each(function(){
+                var li = $(this).find("li");
+                var tul = $(this);
+                $(li).each(function(){
+                        $(this).replaceWith('<tr><td>'+$(this).html()+'</td></tr>');
+                });
+                $(tul).replaceWith('<table>'+$(tul).html()+'</table>');
+        });
+
+	$('[href^="home.php?m=news&p=admin_news&page=edit&id="]').each(function(){
+		$(this).replaceWith('<a href="'+$(this).attr('href')+'"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>');
+	});
+
+	$('img[src="modules/news/images/cancel.gif"]').parent('a').each(function(){
+		$(this).replaceWith('<a href="'+$(this).attr('href')+'" class="btn btn-sm btn-danger pull-right">'+$(this).find('img').attr('alt')+'</a>');
+	});
+
 });
+
+
+function create_button(elem, clss){
+	$(elem).each(function(){
+                $(this).replaceWith('<a href="'+$(this).attr('href')+'" class="btn btn-sm '+clss+' pull-right">'+$(this).text()+'</a>');
+        });
+
+}
--- SBS/modules/news/news.js
+++ SBS/modules/news/news.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
 	$('.news-btn').addClass('btn').addClass('btn-primary').addClass('btn-sm');
+	$('.news-img').removeClass('pull-left');
 });
