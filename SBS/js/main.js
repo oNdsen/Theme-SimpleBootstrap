c4fe384bfb369f37122a175903a0efdb5982ee9f
oNdsen <a.reinhard@me.com>
2017-05-19 21:13:25 +0200

    Removed given JS Codes from global.js in main.js

--- SBS/js/main.js
+++ SBS/js/main.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
 	$('.logo h2').text($('.logo h2').text().substring(0, $('.logo h2').text().lastIndexOf(" [") + 1));
 
 	/* *** Remove CSS and JS Files *** */
-	$('link[href="css/global.css"], script[src="js/global.js"]').remove();
+	$('link[href="css/global.css"]').remove();
+	//$('link[href="css/global.css"], script[src="js/global.js"]').remove();
 	$('link[href="js/bootstrap/css/bootstrap-combined.min.css"]').remove();
 
 	/* *** Removing Chars from Links *** */
@@ -77,7 +78,6 @@ $(document).ready(function() {
 	});
 
 
-	/* *** Pagination *** */
         /* *** Pagination *** */
         $('#pagination').each(function(){
                 $(this).replaceWith('<ul class="pagination">'+$(this).html()+'</ul>');
@@ -136,14 +136,6 @@ $(document).ready(function() {
 	});
 	$('[data-toggle="tooltip"]').each(function(){
 		$(this).tooltip();
-	});
-
-	$("span.versionInfo").click(function(e){
-		handleVersionClick();
-	});
-
-	$(".getAutoUpdateLink").click(function(e){
-		showSteamUpdateLink($(this));
 	});
 
 
@@ -215,128 +207,3 @@ $(document).ready(function() {
 
 
 });
-
-
-
-function showSteamUpdateLink(elem){
-	$("div.mangificWrapper .magnificTitle").text($(elem).attr('autoupdatetext'));
-	$("div.mangificWrapper .magnificContentsDiv").html('<p>' + $(elem).attr('autoupdatehtml') + '</p><p><input class="updateLink" style="width: 75%;" type="text" value="' + $(elem).attr('autoupdatelink') + '"><button class="copyButton">' + $(elem).attr('copyme') + '</button>&nbsp; <span class="copyStatus"></span></p>');
-
-	showPopup(function(){
-		$("input.updateLink").click(function(e){
-			$(this).select();
-		});
-
-		$(".copyButton").click(function(e){
-			copyInput($("input.updateLink"), $("span.copyStatus"), elem);
-		});
-
-		copyInput($("input.updateLink"), $("span.copyStatus"), elem);
-	});
-}
-
-function copyInput(input, resultArea, elemWithAttr){
-	$(input).select();
-	var successful = document.execCommand('copy');
-	var msg = successful ? 'successful' : 'unsuccessful';
-	logToConsole('Copying text command was ' + msg);
-	if(successful){
-		$(resultArea).text($(elemWithAttr).attr('copysuccess')).fadeIn('fast').delay(500).fadeOut('slow').delay(500).fadeIn('slow').delay(500).fadeOut('slow').delay(500).fadeIn('fast').delay(2000).fadeIn('fast');
-	}else{
-		$(resultArea).text($(elemWithAttr).attr('copyfail')).fadeIn('fast').delay(500).fadeOut('slow').delay(500).fadeIn('slow').delay(500).fadeOut('slow').delay(500).fadeIn('fast').delay(2000).fadeIn('fast');
-	}
-}
-
-function showPopup(onOpen, onClose){
-	$.magnificPopup.open({
-		items: {
-			src: $("div.mangificWrapper div.white-popup"),
-			type: 'inline'
-		},
-		callbacks: {
-			close: function() {
-				cleanupPopup();
-				if(!isUndefinedOrEmptyValue(onClose) && jQuery.isFunction(onClose)){
-					onClose();
-				}
-			},
-			open: function() {
-				if(!isUndefinedOrEmptyValue(onOpen) && jQuery.isFunction(onOpen)){
-					onOpen();
-				}
-			}
-		}
-	});
-}
-
-function cleanupPopup(){
-	$("div.mangificWrapper .magnificTitle").text('');
-	$("div.mangificWrapper .magnificContentsDiv").html('');
-}
-
-function handleVersionClick(){
-	if($("span.versionNumber").hasClass("hide")){
-		$("span.versionNumber").removeClass("hide");
-		$("span.version").removeClass("hide");
-
-		// Copy the value of the version if their browser supports it
-		$(document.body).append("<input class='tempCopyValue hide' type='text' value='" + $("span.versionNumber").text() + "' />");
-		try {
-			$("input.tempCopyValue").removeClass("hide");
-			$("input.tempCopyValue").select();
-			var successful = document.execCommand('copy');
-			$("input.tempCopyValue").remove();
-			var msg = successful ? 'successful' : 'unsuccessful';
-			logToConsole('Copying text command was ' + msg);
-			if(successful){
-				$("span.copyVersionResult").text("Copied!").css("color", "#43ff0f").removeClass("hide");
-				$("span.copyVersionResult").css("left", $("span.versionNumber").offset().left + $("span.versionNumber").width() + 5 + "px");
-				$("span.copyVersionResult").fadeIn('fast', function(e){  hideVLength(); }).delay(500).fadeOut('slow', function(e){  showVLength(); }).delay(500).fadeIn('slow', function(e){  hideVLength(); }).delay(500).fadeOut('slow', function(e){  showVLength(); }).delay(500).fadeIn('fast', function(e){  hideVLength(); }).delay(2000).fadeIn('fast',function() {
-					resetVersionView(true);
-				});
-			}
-		}catch(err){
-			resetVersionView(true);
-		}
-	}else{
-		$("span.copyVersionResult").stop();
-		resetVersionView();
-	}
-}
-
-function logToConsole(msg){
-	window.console && console.log(msg);
-}
-
-function isUndefinedOrEmptyValue(input, canBeFalse){
-	if(typeof input === typeof undefined || input === null || input === ''){
-		return true;
-	}
-
-	if(input === false && !isUndefinedOrEmptyValue(canBeFalse) && canBeFalse === true){
-		return false;
-	}else if(input === false){
-		return true;
-	}
-
-	return false;
-}
-
-function resetVersionView(hideVersion){
-	$("input.tempCopyValue").remove();
-	$("span.copyVersionResult").text("");
-	if(isUndefinedOrEmptyValue(hideVersion)){
-//		$("span.versionNumber").removeClass("hide").addClass("hide");
-//		$("span.version").removeClass("hide").addClass("hide");
-	}
-//	$("span.copyVersionResult").removeClass("hide").addClass("hide");
-}
-
-function hideVLength(){
-	$("span.versionNumberCopyLengthener").removeClass("hide").addClass("hide");
-}
-
-function showVLength(){
-	$("span.versionNumberCopyLengthener").removeClass("hide");
-}
-
