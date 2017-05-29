ef79053fe457d69e537c661eb4594c7a409a70d0
oNdsen <ondsen@ondsens-macbook-air.home>
2017-05-29 10:08:36 +0200

    Added Custom BG changer

--- SBS/conf/write_conf.php
+++ SBS/conf/write_conf.php
@@ -1,19 +1,68 @@
 <?php
-if(!empty($_GET['style']))
-{
-	$f = "theme.config";
-	if(file_exists($f)){ unlink($f); }
-	$json = json_encode(array('style' => $_GET['style']));
-	file_put_contents($f, $json);
 
-	$stylesheet = "../css/main.css";
-	$style_file = "../css/main_".$_GET['style'].".css";
+$f = "theme.config";
+$json = json_decode(file_get_contents($f));
 
-	$string = file_get_contents($stylesheet);
-	$replace = file_get_contents($style_file);
+$style = $json->{"style"};
+$bg_conf = $json->{"custom_bg"};
+
+$stylesheet = "../css/main.css";
+if(!empty($_GET['del_custom_bg'])){
+	$style_file = "../css/main_".$style.".css";
+}else{
+	$style_file = "../css/main_".$_POST['style_tab'].".css";
+}
+
+$string = file_get_contents($stylesheet);
+$replace = file_get_contents($style_file);
+
+$cbgf = realpath(dirname(__FILE__)).'/custom_bg';
+if(!file_exists($cbgf)){
+	mkdir($cbgf, 0777, true);
+}
+
+if(!empty($_GET['del_custom_bg'])){
+	if($bg_conf!='no'){
+		unlink($cbgf.'/'.$bg_conf);
+		if(file_exists($f)){ unlink($f); }
+		$json = json_encode(array('style' => $style, 'custom_bg' => 'no'));
+		file_put_contents($f, $json);
+	}
 	$replace = "/* *** THEME STYLER *** */\n".$replace."\n/* *** THEME STYLER END *** */";
 	$search = "/\/\* \*\*\* THEME STYLER \*\*\* \*\/(.*)\/\* \*\*\* THEME STYLER END \*\*\* \*\//s";
-
 	file_put_contents($stylesheet, preg_replace($search,$replace,$string));
 
+	exit();
 }
+
+if(!empty($_FILES)){
+	if($_FILES['bg_file']['error']==0){
+
+		$validextensions = array("jpeg", "jpg", "png");
+		$temporary = explode(".", $_FILES["bg_file"]["name"]);
+		$file_extension = end($temporary);
+		$file_name = 'custom.'.$file_extension;
+		$file_complete = $cbgf.'/'.$file_name;
+		if ((($_FILES["bg_file"]["type"] == "image/png") || ($_FILES["bg_file"]["type"] == "image/jpg") || ($_FILES["bg_file"]["type"] == "image/jpeg")) && in_array($file_extension, $validextensions)) {
+			if ($_FILES["file"]["error"]==0){
+				if(file_exists($file_complete)){ unlink($file_complete); }
+				move_uploaded_file($_FILES['bg_file']['tmp_name'],$file_complete);
+				$bg_conf = $file_name;
+			}
+		}
+	}
+}
+
+if(file_exists($f)){ unlink($f); }
+$json = json_encode(array('style' => $_POST['style_tab'], 'custom_bg' => $bg_conf));
+file_put_contents($f, $json);
+
+if($bg_conf!='no'){
+	$replace = preg_replace("/background: url\((.*)\)/", "background: url(../conf/custom_bg/".$bg_conf.")", $replace);
+}
+$replace = "/* *** THEME STYLER *** */\n".$replace."\n/* *** THEME STYLER END *** */";
+$search = "/\/\* \*\*\* THEME STYLER \*\*\* \*\/(.*)\/\* \*\*\* THEME STYLER END \*\*\* \*\//s";
+file_put_contents($stylesheet, preg_replace($search,$replace,$string));
+
+
+?>
--- SBS/modules/settings/themes.js
+++ SBS/modules/settings/themes.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
+	$.ajaxSetup({ cache: false });
+
 	$('form[action="?m=settings&p=themes"] tr:nth-child(4),form[action="?m=settings&p=themes"] tr:nth-child(5)').addClass('hide');
+	$('form[action="?m=settings&p=themes"]').attr('enctype','multipart/form-data');
 
         $.getJSON("themes/SBS/conf/theme.config", function(json) {
                 if(json['style']=='light'){
@@ -8,23 +11,69 @@ $(document).ready(function() {
                 }else{
 			$('form[action="?m=settings&p=themes"] tr:last').after('<tr><td align="right"><label for="style_tab">Theme Style:</label></td><td align="left"><select id="style_tab" name="style_tab" class="form-control"><option value="dark" selected>dark</option><option value="light">light</option></select></td><td><i class="fa fa-question-circle-o" aria-hidden="true" data-toggle="tooltip" data-placement="left" title="Changes the Theme Style"></i></td></tr>');
                 }
+
+		if(json['custom_bg']=='no'){
+			var custom_bg_info = '<div class="label label-danger show">BG not set</div>';
+		}else{
+			var custom_bg_info = '<div class="label label-success show">BG set <a class="btn btn-xs btn-danger" id="del_custom_bg"><i class="fa fa-trash" aria-hidden="true"></i></a></div>';
+		}
+
+		var upload_input = '\
+		<div class="input-group">\
+			<label class="input-group-btn">\
+				<span class="btn btn-primary">\
+					Browse… <input type="file" style="display: none;" name="bg_file" id="bg_file">\
+				</span>\
+			</label>\
+			<input type="text" class="form-control" readonly="">\
+		</div>';
+		$('form[action="?m=settings&p=themes"] tr:last').after('<tr><td align="right"><label for="custom_bg">Custom BG:'+custom_bg_info+'</label></td><td align="left">'+upload_input+'</td><td><i class="fa fa-question-circle-o" aria-hidden="true" data-toggle="tooltip" data-placement="left" title="Changes the Theme Background"></i></td></tr>');
         });
 
-
-	$('input[name="update_settings"]').click(function(){
-		var theme_style = $('#style_tab').val();
+	$('form[action="?m=settings&p=themes"]').submit(function(e){
+//		e.preventDefault();
 		$.ajax({
-			url: 'themes/SBS/conf/write_conf.php',
-			type: 'GET',
-			data: {style:theme_style},
-			success: function(data) {
-				//console.log('success');
+			url: "themes/SBS/conf/write_conf.php",
+			type: "POST",
+			data: new FormData(this),
+			contentType: false,
+			cache: false,
+			processData:false,
+			success: function(data){
 				//console.log(data);
-			},
-			error: function(log) {
-				console.log(log.message);
 			}
 		});
-		//return false;
+
 	});
+
+});
+
+$(window).load(function(){
+        $('#del_custom_bg').click(function(){
+              $.get('themes/SBS/conf/write_conf.php',
+                        {del_custom_bg:'yes'},
+                        function(data){
+                                console.log(data);
+                        }
+                );
+                location.reload();
+        });
+
+	$(':file').on('fileselect', function(event, numFiles, label) {
+		var input = $(this).parents('.input-group').find(':text'),
+		log = numFiles > 1 ? numFiles + ' files selected' : label;
+		if(input.length){
+			input.val(log);
+		}else{
+			if(log) alert(log);
+		}
+	});
+});
+
+
+$(document).on('change', ':file', function() {
+	var input = $(this),
+	numFiles = input.get(0).files ? input.get(0).files.length : 1,
+	label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+	input.trigger('fileselect', [numFiles, label]);
 });
